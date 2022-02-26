77dd22370de39030e42a86ce1048ba3e80778dd4
John Hur <hur.john628@gmail.com>
2022-02-26 01:05:06 -0600

    added pics and some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import './App.css'
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './header/Header';
+import Image from './image/Image'
 import Footer from './footer/Footer';
 import AboutMe from './aboutMe/AboutMe';
 import Portfolio from './portfolio/Portfolio';
@@ -7,15 +10,19 @@ import Contact from './contact/Contact';
 import Resume from './resume/Resume';
 
 function App() {
+  const [ image, setImage ] = useState('')
   return (
-    <div className="">
+    <div className="da-body">
       <Header />
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <div className='container'>
+        <Image />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/image/Image.js
@@ -0,0 +1,9 @@
+import './Image.css'
+
+export default function Image() {
+    return (
+        <div className="image">
+            IMAGE!
+        </div>
+    )
+}
