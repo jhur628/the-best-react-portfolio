0e627a3de2581a4dc71e25667e123c905cb55e26
John Hur <hur.john628@gmail.com>
2022-02-27 06:31:17 -0600

    added about me text and added some styling

--- src/aboutMe/AboutMe.js
+++ src/aboutMe/AboutMe.js
@@ -1,9 +1,25 @@
-
+import './AboutMe.css'
 
 export default function AboutMe() {
     return (
-        <div>
-            Hey!
+        <div className='about-me-container'>
+            <h1>About Me</h1>
+            <p>
+                My name is John Hur. Born in Germany June 28, 1990, I began my crazy existence.
+            </p>
+            <p>
+                I worked in social services and counseling for about seven years. It was rewarding, but at the same time, it was very taxing. I am very grateful for the experience and lessons within the field.
+                I learned how to be more disciplines, how to be part of a team, how to learn, how to problem solve, and how to move forward.
+            </p>
+            <p>
+                I have always wanted to learn to code, and 2021, I made the first step towards becoming a full stack developer. I enrolled in Northwestern's coding full-stack bootcamp. I realized immediately this is where I belong.
+                It was challenging, but I pushed and developed, both my applications and my character as a coder. I could have been an average student, and coasted through the bootcamp in the middle. Instead, I joined a study group where my peers and friends pushed me, helped eliminate inadequacies, and provided support.
+            </p>
+            <p>
+                I am proud of my applications. I want to be able to continue to be proud of what I do. I want to create, grow, work with a great team, help grow a great team into a greater team, and I want to continue to move forward.
+                This is where I belong, and I'm just happy to be here.
+            </p>
+            
         </div>
     )
 }
