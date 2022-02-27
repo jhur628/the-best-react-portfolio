492a389f3523910d9142fa74d56e805cbcf80efa
John Hur <hur.john628@gmail.com>
2022-02-27 07:21:07 -0600

    Working on footer, added gh-pages

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,11 @@
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
         <footer>
-            <p>This is a footer!</p>
+            <a href="https://github.com/jhur628" target="_blank"></a>
         </footer>
     )
 }
